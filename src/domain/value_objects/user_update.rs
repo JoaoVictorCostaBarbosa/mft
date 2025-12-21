d9a1ea71668f0d6b2a0c1990d0bc76cf71512364
Joao Victor <joaovictorcostabarbosac@gmail.com>
2025-12-21 10:40:11 -0300

    remove: user_update VO

--- src/domain/value_objects/user_update.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug, Default, Clone)]
-pub struct UserUpdateFilds {
-    pub name: Option<String>,
-    pub email: Option<String>,
-    pub password: Option<String>,
-    pub url_img: Option<String>,
-}
